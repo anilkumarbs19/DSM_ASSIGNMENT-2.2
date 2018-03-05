2e36dc0f24108b24dbc856a4fffb31e96924c98e
anilkumarbs19 <anilkumarbs19@gmail.com>
2018-03-05 20:53:23 +0530

    assignment2.2

---
+++ Assignment 2.2.py
@@ -0,0 +1,16 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+for x in range(0,5):
+    for i in range(0,x+1):
+        print("*",end="")
+    print("\r")
+n = 5
+for i in range(1, 5):
+    for j in range(0,n-i):
+        print("*", end="")
+    print("\r")
+
